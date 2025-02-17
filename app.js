27f04204485e8e749d4e8f1337c151fc2f77d641
Vinicius Freitas <vinixgon@gmail.com>
2025-02-17 16:38:01 -0300

    Implementing deep prompt, prompt and cached history visualization

--- app.js
+++ app.js
@@ -2,13 +2,31 @@ const express = require('express');
 const app = express()
 const port = 3000
 
+// Calling the integraton.js file
+const integration = require('./integration');
+
+// Middleware to interpret JSON in the request body
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello Word!')
 });
 
-app.get('/api/returnPrompt', (req, res) => {
-  // Function to view user prompt
+// API - Return deep prompt, prompt and history chat
+app.post('/api/returnPrompt', async (req, res) => {
+  const data = await integration.returnPrompt(req, res);
+  res.json(data);
 });
 
+// API - Return history cache
+app.post('/api/viewHistory', (req, res) => {
+  integration.viewHistory(req, res);
+});
+
+// API - Return QRCode for WhastApp
+app.post('/api/returnqr', async (req, res) => {
+  const data = await integration.returnqr(req, res);
+  res.json(data);
+});
 
 app.listen(port, () => console.log(`deepseek-with-whatsapp listening on http://localhost:${port}/ `))
--- integration.js
+++ integration.js
@@ -1,15 +1,45 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode');
+const axios  = require('axios');
 
+// Deep prompt for entire application
+const deepPrompt = 'Responda de forma natural e amigável, mantendo o contexto da conversa.'
 
-exports.processMessage = async (req, res) => {
+// Object to store conversation history
+let history = {}
+
+// Function - Return QRCode for WhastApp
+exports.returnqr = async (req, res) => {
   try {
+    res.send('Return QRCode is working!');
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-    // INPUTS DATAS
-    prompt = req.body.text;
+// Function - Return history cache
+exports.returnPrompt = async (req, res) => {
+  try {
+    const userPrompt = req.body.prompt;
 
-    // Return function
-    res.json(prompt);
+    const returnData = {
+        prompt     : userPrompt
+      , deepPrompt : deepPrompt      
+      , history    : history
+    }
 
-  } catch (err) {
-    res.status(500).json({ error: error.message })
+    return returnData;
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+// Function - Return deep prompt, prompt and history chat
+exports.viewHistory = async (req, res) => {
+  try {
+    res.send(history);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
